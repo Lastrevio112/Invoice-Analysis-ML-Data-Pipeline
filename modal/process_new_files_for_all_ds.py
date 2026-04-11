db1bf7dc0746fa0a90f141b6b6483ed377e36606
Lastrevio <stefanzingzing@yahoo.com>
2026-04-11 10:58:14 +0000

    added 520 file limit in modal orchestrator to avoid github actions timeout

--- modal/process_new_files_for_all_ds.py
+++ modal/process_new_files_for_all_ds.py
@@ -29,11 +29,16 @@ def processNewFilesInDatasource(DataSourceId: int):
     bucket_name = f"ds{DataSourceId}"
     bigquery_destination_table = f"invoiceanalysispipeline.bronze.ds_{DataSourceId}_raw_json"
 
+    MAX_FILES_PER_RUN = 520 # GitHub actions has a max runtime of 6 hours, and based on testing, processing each file takes around 30-40 seconds on average. So we can process around 540 files in one run to avoid timeouts, but to be safe we set the limit a bit lower.
+
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix="unprocessed/")
-    files = [obj["Key"] for obj in response.get("Contents", []) if obj["Key"].endswith((".jpg", ".jpeg", ".png", ".pdf"))]
+    files = [
+        obj["Key"] for obj in response.get("Contents", []) 
+        if obj["Key"].endswith((".jpg", ".jpeg", ".png", ".pdf"))
+        ][:MAX_FILES_PER_RUN]
 
     if not files:
-        print("No new files to process for datasource ", DataSourceId)
+        print("No new files to process for datasource", DataSourceId)
         return
 
     for key in files:
